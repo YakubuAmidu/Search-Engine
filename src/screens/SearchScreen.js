9f77975b278a658159133d2677b30b7a71e36068
Yakubu Amidu <yakubuamidu@Yakubus-MacBook-Pro.local>
2020-04-03 11:24:05 -0700

    Commiting an app

---
+++ src/screens/SearchScreen.js
@@ -0,0 +1,36 @@
+import React, { useState } from 'react';
+import { Text, View, StyleSheet, ScrollView } from 'react-native';
+import SearchBar from '../components/SearchBar';
+import useResults from '../hooks/useResults';
+import ResultsList from '../components/ResultsList';
+
+const SearchScreen = () => {
+const [term, setTerm ] = useState('');
+const [searchApi, results, errorMessage] = useResults();
+
+const filterResultsByPrice = (price) => {
+  return results.filter(result => {
+    return result.price === price;
+  });
+};
+
+    return (
+        <>
+            <SearchBar
+            term = {term}
+            onTermChange={setTerm}
+             onTermSubmit={() => searchApi(term)}
+            />
+            {errorMessage ? <Text>{errorMessage}</Text> : null}
+            <ScrollView>
+             <ResultsList  results={filterResultsByPrice('$')} title="Cost Effect"/>
+              <ResultsList  results={filterResultsByPrice('$$')} title="Bit Pricier"/>
+               <ResultsList  results={filterResultsByPrice('$$')} title="Big Spender"/>
+               </ScrollView>
+        </>
+    );
+};
+
+const styles = StyleSheet.create({});
+
+export default SearchScreen;
