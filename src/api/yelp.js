dbfd59af5ba9b0f8b45370d3b6f51cafc12f681d
Yakubu Amidu <yakubu.amidu@yahoo.com>
2020-06-29 00:07:20 -0700

    Commiting changes

--- src/api/yelp.js
+++ src/api/yelp.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export default axios.create({
-  baseURL: 'https://api.yelp.com/v3/businesses',
+  baseURL:  'https://api.yelp.com/v3/businesses',
   headers: {
     Authorization: 'Bearer peVSxo2PDw0AVDIZcznTAYsa0UVhtIwLB5VCcIIb12IjKeGY__KGNbhMThfLoNrm0prTKJnbGuh3PhU0D0cISpxUgNsiBaWCFU9o27LuwB_mO9ddRgn4NBuJA3FSXXYx'
   }
