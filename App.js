b3468f2881c9052758b1929828e32c38d442b3f5
Yakubu Amidu <yakubu.amidu@yahoo.com>
2020-06-29 00:18:47 -0700

    Commiting changes

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import ResultsShowScreen from './src/screens/ResultsShowScreen';
 
 const navigator = createStackNavigator({
   Search: SearchScreen,
-  ResultsShow: ResultsShowScreen
+  ResultsShow: 
 }, {
   initialRouteName: 'Search',
   defaultNavigationOptions: {
