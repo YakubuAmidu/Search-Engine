cb81911c006e27439761ea7e43198cd935d7b0aa
Yakubu Amidu <yakubu.amidu@yahoo.com>
2020-06-29 00:16:06 -0700

    Comiiting changes

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import { createAppContainer } from 'react-navigation';
-import { createStackNavigator  } from 'react-navigation-stack';
+import { createStackNavigator } from 'react-navigation-stack';
 import SearchScreen from './src/screens/SearchScreen';
 import ResultsShowScreen from './src/screens/ResultsShowScreen';
 
