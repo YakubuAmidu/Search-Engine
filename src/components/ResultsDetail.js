43c65221e04c20dde421d1ee0f0a158bb9dedeb0
Yakubu Amidu <yakubu.amidu@yahoo.com>
2020-06-29 00:10:10 -0700

    Commiting changes

--- src/components/ResultsDetail.js
+++ src/components/ResultsDetail.js
@@ -3,7 +3,7 @@ import { View, Image, Text, StyleSheet } from 'react-native';
 
 const ResultsDetail = ({ result }) => {
   return (
-    <View  style={styles.container}>
+    <View style={styles.container}>
   <Image style={styles.image} source={{ uri: result.image_url}} />
   <Text style={styles.name}>{result.name}</Text>
   <Text>{result.rating} Stars, {result.review_count} Reviews</Text>
