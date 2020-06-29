e3c694c376c6ccbad274939df24d34150c10ff9b
Yakubu Amidu <yakubu.amidu@yahoo.com>
2020-06-29 00:22:52 -0700

    Commiting changes

--- App.js
+++ App.js
@@ -7,3 +7,10 @@ const navigator = createStackNavigator({
   Search: SearchScreen,
   ResultsShow: ResultsShowScreen
 }, {
+  initialRouteName: 'Search',
+  defaultNavigationOptions: {
+    title: 'Business Search'
+  }
+});
+
+export default createAppContainer(navigator);
